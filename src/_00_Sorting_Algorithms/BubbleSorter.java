10067ea92ee7a200adad55b7ac98ce5896cbcae6
Kaiya <league@WTS14.attlocal.net>
2018-07-01 17:27:10 -0700

    Sorting Algorithms IP

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -1,16 +1,26 @@
 package _00_Sorting_Algorithms;
 
-public class BubbleSorter extends Sorter{
+public class BubbleSorter extends Sorter {
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
-	//1. Use the bubble sorting algorithm to sort the array.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// 1. Use the bubble sorting algorithm to sort the array.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		for (int j = 0; j < array.length; j++) {
+			for (int i = 0; i < array.length - 1; i++) {
+				if (array[i] > array[i + 1]) {
+					int first = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = first;
+					display.updateDisplay();
+				}
+			}
+		}
+
 	}
-	
+
 }
